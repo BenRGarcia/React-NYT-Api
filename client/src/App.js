81c4f0f21276c47ff4926c99caaf312a922e4978
Ben Garcia <ben@seebenprogram.com>
2018-06-07 17:24:10 -0400

    Remove boilerplate code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { HelloWorldContainer } from './containers/HelloWorldContainer';
 
 class App extends Component {
   render() {
     return (
-      <HelloWorldContainer />
+      <div>
+        Hello world
+      </div>
     );
   }
 }
