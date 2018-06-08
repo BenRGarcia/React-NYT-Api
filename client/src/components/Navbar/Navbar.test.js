59a408d325b11c17e562376fd5c58bb0feeed680
Ben Garcia <ben@seebenprogram.com>
2018-06-07 22:35:18 -0400

    write first tests

---
+++ client/src/components/Navbar/Navbar.test.js
@@ -0,0 +1,34 @@
+import { Navbar } from './Navbar';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Navbar.js', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Navbar />
+    );
+  });
+
+  it('should have a `nav` element', () => {
+    expect(
+      wrapper.find('nav').length
+    ).toBe(1);
+  });
+
+  it('should have 2 react-router-dom `Link` elements', () => {
+    expect(
+      wrapper.find('Link').length
+    ).toEqual(2)
+  });
+
+  it('First `Link` should have an `active` CSS class', () => {
+    const activeLink = wrapper.find('Link').first();
+    expect(
+      activeLink.hasClass('active')
+    ).toBe(true);
+  });
+});
