b9199d8e011af5cc09092385dadc34e9a41d7a06
Ben Garcia <ben@seebenprogram.com>
2018-06-08 15:57:47 -0400

    build hard coded component

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 export const Search = props => (
-  <div>
-    Search
+  <div className="card bg-light mb-4">
+    <div className="card-header h2">Search for Articles</div>
+    <div className="card-body">
+      <div className="card-text">
+        <form>
+          <label className="card-title h5 d-block mb-1">
+            Topic
+          </label>
+          <input type="text" className='d-block w-75 mx-auto mb-3' />
+          <label className="card-title h5 d-block mb-1">
+            Start Year
+          </label>
+          <input type="text" className='d-block w-75 mx-auto mb-3' />
+          <label className="card-title h5 d-block mb-1">
+            End Year
+          </label>
+          <input type="text" className='d-block w-75 mx-auto mb-3' />
+          <button type="submit" className="btn btn-secondary btn-lg btn-block w-25 mx-auto">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 );
