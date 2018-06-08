29ceca28fc0e9d429b81432d817ea72b870ce610
Ben Garcia <ben@seebenprogram.com>
2018-06-07 22:35:28 -0400

    pass initial tests

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-export const Navbar = () => (
-  <div>Navbar</div>
+const linkCSSClass = path => (
+  window.location.pathname === path
+    ? 'nav-link active'
+    : 'nav-link'
 );
+
+export const Navbar = () => {
+  return (
+    <nav className='nav justify-content-center'>
+      <Link
+        to='/'
+        className={linkCSSClass("/")}
+      >
+        Search Articles
+      </Link>
+      <Link
+        to='/saved'
+        className={linkCSSClass("/saved")}
+      >
+        Saved Articles
+      </Link>
+    </nav>
+  );
+};
