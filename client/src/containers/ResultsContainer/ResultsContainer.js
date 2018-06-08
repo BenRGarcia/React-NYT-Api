6c65511507ad9ebb981d71c40b62e0697e5fffa6
Ben Garcia <ben@seebenprogram.com>
2018-06-08 15:57:59 -0400

    initial commit

---
+++ client/src/containers/ResultsContainer/ResultsContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Results } from '../../components/Results/Results';
+
+export class ResultsContainer extends Component {
+  state = {};
+  
+  render() {
+    return (
+      <Results />
+    );
+  }
+}
