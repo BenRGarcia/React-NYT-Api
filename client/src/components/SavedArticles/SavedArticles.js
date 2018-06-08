cb9b92556896f9e49daeca3a76f4be8593912514
Ben Garcia <ben@seebenprogram.com>
2018-06-08 16:20:14 -0400

    build hard coded initial state component

--- client/src/components/SavedArticles/SavedArticles.js
+++ client/src/components/SavedArticles/SavedArticles.js
@@ -1,7 +1,97 @@
 import React from 'react';
 
 export const SavedArticles = props => (
-    <div>
-      SavedArticles
+  <div className="card bg-light mb-4">
+    <div className="card-header h2 mb-3">
+      Saved Articles
     </div>
+    {/* Insert Ternary statement here */}
+    <div className='mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-center py-2">
+        <span className="card-text">
+          Saved Articles Appear Here
+        </span>
+      </div>
+    </div>
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text h4">
+          Hard Coded Article Title 1
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Remove
+        </button>
+      </div>
+      <form className='px-3'>
+        <label className='mb-0'>Notes:</label>
+        <textarea name="" id="" className='w-100' style={{ height: '50px' }} value='hello world'></textarea>
+      </form>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text h4">
+          Hard Coded Article Title 2
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Remove
+        </button>
+      </div>
+      <form className='px-3'>
+        <label className='mb-0'>Notes:</label>
+        <textarea name="" id="" className='w-100' style={{ height: '50px' }}></textarea>
+      </form>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text h4">
+          Hard Coded Article Title 3
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Remove
+        </button>
+      </div>
+      <form className='px-3'>
+        <label className='mb-0'>Notes:</label>
+        <textarea name="" id="" className='w-100' style={{ height: '50px' }}></textarea>
+      </form>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text h4">
+          Hard Coded Article Title 4
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Remove
+        </button>
+      </div>
+      <form className='px-3'>
+        <label className='mb-0'>Notes:</label>
+        <textarea name="" id="" className='w-100' style={{ height: '50px' }}></textarea>
+      </form>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text h4">
+          Hard Coded Article Title 5
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Remove
+        </button>
+      </div>
+      <form className='px-3'>
+        <label className='mb-0'>Notes:</label>
+        <textarea name="" id="" className='w-100' style={{ height: '50px' }}></textarea>
+      </form>
+    </div>
+    {/* map end: */}
+  </div>
 );
