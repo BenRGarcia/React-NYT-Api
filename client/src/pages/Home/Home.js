60ac47b0ecb161120fb90999fd852c937ee012e0
Ben Garcia <ben@seebenprogram.com>
2018-06-08 15:57:04 -0400

    reorg component tree

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { SearchContainer } from '../../containers/SearchContainer/SearchContainer';
 import { SearchResultsContainer } from '../../containers/SearchResultsContainer/SearchResultsContainer';
 
 export const Home = () => (
   <div>
-    <SearchContainer />
     <SearchResultsContainer />
   </div>
 );
