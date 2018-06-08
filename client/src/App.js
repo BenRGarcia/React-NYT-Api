3f5e915bde183d5d1dfb14dfeebbfe65f3e71fcd
Ben Garcia <ben@seebenprogram.com>
2018-06-07 20:27:06 -0400

    Import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Navbar } from './components/Navbar/Navbar';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+import { Home } from './pages/Home';
+import { Saved } from './pages/Saved';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello world
+        <Navbar />
+        <Header />
+        <Router>
+          <div>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/saved' component={Saved} />
+          </div>
+        </Router>
+        <Footer />
       </div>
     );
   }
