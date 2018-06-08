9c4cc3cb0b977ba525664418babfd0cc0e0f34dc
Ben Garcia <ben@seebenprogram.com>
2018-06-08 15:57:37 -0400

    build hard coded initial state

---
+++ client/src/components/Results/Results.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+export const Results = props => (
+  <div className="card bg-light mb-4">
+    <div className="card-header h2 mb-3">
+      Search Results
+    </div>
+    {/* Insert Ternary statement here */}
+    <div className='mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-center py-2">
+        <span className="card-text">
+          Article Search Results Appear Here
+        </span>
+      </div>
+    </div>
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text">
+          Hard Coded Article Title 1
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Save
+        </button>
+      </div>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text">
+          Hard Coded Article Title 2
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Save
+        </button>
+      </div>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text">
+          Hard Coded Article Title 3
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Save
+        </button>
+      </div>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text">
+          Hard Coded Article Title 4
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Save
+        </button>
+      </div>
+    </div>
+    {/* map end: */}
+    {/* map start */}
+    <div className='border border-secondary rounded w-75 mx-auto mb-3'>
+      <div className="card-body d-flex justify-content-between align-items-center py-2">
+        <span className="card-text">
+          Hard Coded Article Title 5
+        </span>
+        <button type='button' className='btn btn-secondary'>
+          Save
+        </button>
+      </div>
+    </div>
+    {/* map end: */}
+  </div>
+);
