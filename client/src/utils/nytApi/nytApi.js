5b38ac3d03fcda086171977dee04b82f08f8bc95
Ben Garcia <ben@seebenprogram.com>
2018-06-08 21:31:13 -0400

    build initial idea of nyt api call

--- client/src/utils/nytApi/nytApi.js
+++ client/src/utils/nytApi/nytApi.js
@@ -1,3 +1,15 @@
-export const nytApiSearch = async ({ topic, startYear, endYear }) => {
-  return 'call NYT api from here';
+export const generateNytApiQueryString = ({ topic, startYear, endYear }) => {
+  let url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?`;
+  url += `api-key=8a65f491cc5a4cb085f9fe3fb7263e7a`;
+  url += `&q=${topic}`;
+  url += `&begin_date=${startYear}0101`;
+  url += `&end_date=${endYear}1231`;
+  url += `&fl=headline,web_url,pub_date`;
+  return url;
+};
+
+export const nytApiSearch = async (queryString) => {
+  const resp = await fetch(queryString);
+  const data = await resp.json();
+  return data.response.docs;
 };
